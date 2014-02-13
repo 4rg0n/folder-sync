0566f11be2b9d7e5b0513fd2fab987834707e287
arg0n <arg0n@sandboxer-online.de>
2014-02-13 22:14:08 +0100

    Sichergestellt, dass nur Models zur Collection hinzugefügt werden können

--- src/OST/Collection/FileCollection.php
+++ src/OST/Collection/FileCollection.php
@@ -9,7 +9,30 @@
 
 namespace OST\Collection;
 
+use OST\Model\ModelInterface;
+
 class FileCollection extends AbstractCollection
 {
+    /**
+     * Stellt sicher, dass das Item das korrekte Interface implementiert
+     *
+     * @see OST\Collection\AbstractCollection::add();
+     *
+     * @param int|string $key
+     * @param OST\Model\ModelInterface $item
+     * @return mixed
+     * @throws \InvalidArgumentException
+     */
+    public function add($key, $item)
+    {
+        if (false === $item instanceof ModelInterface) {
+            throw new \InvalidArgumentException(sprintf(
+                'Parameter item muss eine "%s" implementieren. "%s" gegeben',
+                get_class(ModelInterface),
+                gettype($item)
+            ));
+        }
 
+        return parent::add($key, $item);
+    }
 }
