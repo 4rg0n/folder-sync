e1dfe250554d23986a784498632aa50b909a3814
4rg0n <argonthechecker@gmail.com>
2014-02-13 22:33:12 +0100

    Extra Datei- und Ordner Collection angelegt

--- src/OST/Collection/File.php
+++ src/OST/Collection/File.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Ordner Collection
+ * Datei Collection
  *
  * @package OST\Collection
  * @author Dominic Rönicke <argonthechecker@gmail.com>
@@ -9,26 +9,26 @@
 
 namespace OST\Collection;
 
-use OST\Model\ModelInterface;
+use OST\Model\File as FileModel;
 
-class FileCollection extends AbstractCollection
+class File extends AbstractCollection
 {
     /**
-     * Stellt sicher, dass das Item das korrekte Interface implementiert
+     * Stellt sicher, dass das Item eine Datei ist
      *
      * @see OST\Collection\AbstractCollection::add();
      *
      * @param int|string $key
-     * @param OST\Model\ModelInterface $item
+     * @param \OST\Model\File $item
      * @return mixed
      * @throws \InvalidArgumentException
      */
     public function add($key, $item)
     {
-        if (false === $item instanceof ModelInterface) {
+        if (false === $item instanceof FileModel) {
             throw new \InvalidArgumentException(sprintf(
-                'Parameter item muss eine "%s" implementieren. "%s" gegeben',
-                get_class(ModelInterface),
+                'Parameter item muss eine Instanz von "%s" sein. "%s" gegeben',
+                'OST\Model\File',
                 gettype($item)
             ));
         }
